abbd56b67883183e3f678f359d51de5ca9269e61
Tomasz Gospodarczyk <tomasz.gospodarczyk@allegro.pl>
2021-09-29 20:16:58 +0200

    first commit

---
+++ functions/kruk.js
@@ -0,0 +1,57 @@
+
+const { getReports } = require('kruk');
+
+
+function prependHttp(url, {https = true} = {}) {
+	if (typeof url !== 'string') {
+		throw new TypeError(`Expected \`url\` to be of type \`string\`, got \`${typeof url}\``);
+	}
+
+	url = url.trim();
+
+	if (/^\.*\/|^(?!localhost)\w+?:/.test(url)) {
+		return url;
+	}
+
+	return url.replace(/^(?!(?:\w+?:)?\/\/)/, https ? 'https://' : 'http://');
+}
+
+
+
+exports.handler = async (event) => {
+
+const params = new URLSearchParams(event.body);
+const urls = params.getAll('url').map(url => prependHttp(url));
+
+  const API_KEY = $PSIKUS;
+
+  
+  const queryParams = {
+    effectiveConnectionType: '',
+    formFactor: 'PHONE',
+    checkOrigin: true
+  }
+  
+  const cruxData = await getReports(urls, API_KEY, queryParams);
+  
+
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(cruxData),
+  };
+
+};
+
+
+
+
+
+
+
+
+
+
