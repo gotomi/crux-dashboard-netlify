91eca29fb1aff8a022595b06d91c0d1f968359f1
gotomi <gotomi@gotomi.info>
2022-05-04 17:25:50 +0200

    remove netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
-import netlify from '@astrojs/netlify/functions';
 
 
 
 export default defineConfig({
-    adapter: netlify(),
     integrations: [
         svelte(),
     ]
