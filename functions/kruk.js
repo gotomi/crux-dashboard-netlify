316ada6ac70111c385a2ef238a8498eb68283e97
gotomi <gotomi@gotomi.info>
2023-09-22 13:47:52 +0200

    kruk update

--- functions/kruk.js
+++ functions/kruk.js
@@ -1,59 +1,45 @@
+import { getReports } from "kruk";
 
-const { getReports } = require('kruk');
+function prependHttp(url, { https = true } = {}) {
+  if (typeof url !== "string") {
+    throw new TypeError(
+      `Expected \`url\` to be of type \`string\`, got \`${typeof url}\``
+    );
+  }
 
+  url = url.trim();
 
-function prependHttp(url, {https = true} = {}) {
-	if (typeof url !== 'string') {
-		throw new TypeError(`Expected \`url\` to be of type \`string\`, got \`${typeof url}\``);
-	}
+  if (/^\.*\/|^(?!localhost)\w+?:/.test(url)) {
+    return url;
+  }
 
-	url = url.trim();
-
-	if (/^\.*\/|^(?!localhost)\w+?:/.test(url)) {
-		return url;
-	}
-
-	return url.replace(/^(?!(?:\w+?:)?\/\/)/, https ? 'https://' : 'http://');
+  return url.replace(/^(?!(?:\w+?:)?\/\/)/, https ? "https://" : "http://");
 }
 
-
-
 exports.handler = async (event) => {
-
-const params = new URLSearchParams(event.body);
-const checkOrigin = !!params.get('checkOrigin');
-const formFactor = params.get('formFactor');
-const urls = params.getAll('url').filter(item => !!item).map(url => prependHttp(url));
+  const params = new URLSearchParams(event.body);
+  const checkOrigin = !!params.get("checkOrigin");
+  const formFactor = params.get("formFactor");
+  const urls = params
+    .getAll("url")
+    .filter((item) => !!item)
+    .map((url) => prependHttp(url));
 
   const API_KEY = process.env.PSIKUS;
 
-  
   const queryParams = {
-    effectiveConnectionType: '',
+    effectiveConnectionType: "",
     formFactor: formFactor,
-    checkOrigin: checkOrigin
-  }
-  
-  const cruxData = await getReports(urls, API_KEY, queryParams);
-  
+    checkOrigin: checkOrigin,
+  };
 
+  const cruxData = await getReports(urls, API_KEY, queryParams);
 
   return {
     statusCode: 200,
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(cruxData),
   };
-
 };
-
-
-
-
-
-
-
-
-
-
