cd069289624687cccafd5aff83116568e8573cad
gotomi <gotomi@gotomi.info>
2025-03-19 14:15:09 +0100

    udapte, fix labels for RTT

--- src/components/Legend.svelte
+++ src/components/Legend.svelte
@@ -1,66 +1,79 @@
 <script>
- let {metric} = $props();
+    let { metric } = $props();
 
-  const metricsMap = {
-    CLS: { range: [0.1, 0.25], name: "Cumulative Layout Shift" },
-    FCP: { range: [1800, 3000], name: "First Contentful Paint" },
-    LCP: { range: [2500, 4000], name: "Largest Contentful Paint" },
-    TTFB: { range: [800, 1800], name: "Time to First Byte" },
-    INP: { range: [200, 500], name: "Interaction To Next Paint" },
-    RTT: { range: [75, 275], name: "Round Trip Time" },
-  };
+    const metricsMap = {
+        CLS: { range: [0.1, 0.25], name: "Cumulative Layout Shift" },
+        FCP: { range: [1800, 3000], name: "First Contentful Paint" },
+        LCP: { range: [2500, 4000], name: "Largest Contentful Paint" },
+        TTFB: { range: [800, 1800], name: "Time to First Byte" },
+        INP: { range: [200, 500], name: "Interaction To Next Paint" },
+        RTT: { range: [75, 275], name: "Round Trip Time" },
+    };
 
-  const metricData = metricsMap[metric];
-  const unit = metric === "CLS" ? "" : "ms";
+    const metricData = metricsMap[metric];
+    const unit = metric === "CLS" ? "" : "ms";
+    const names = {
+        metrics: {
+            good: "Good",
+            medium: "Needs Improvement",
+            poor: "Poor",
+        },
+        rtt: {
+            good: "Good",
+            medium: "Medium",
+            poor: "Low",
+        },
+    };
+
+    const rangeNames = metric === "RTT" ? names.rtt : names.metrics;
 </script>
 
 <div>
-  <h2 title={metricData.name}>
-    <span class="mobile">
-      {metric}
-    </span>
-    <span class="desktop">{metricData.name} </span>
+    <h2 title={metricData.name}>
+        <span class="mobile">
+            {metric}
+        </span>
+        <span class="desktop">{metricData.name} </span>
     </h2>
-  <p class="legend">
-    <span>🟢 good (&lt; {metricData.range[0]} {unit}) </span>
-    <span>🟠 needs improvement</span>
-    <span>🔴 poor (&gt; {metricData.range[1]}{unit})</span>
-  </p>
+    <p class="legend">
+        <span>🟢 {rangeNames.good} (&lt; {metricData.range[0]} {unit}) </span>
+        <span>🟠 {rangeNames.medium}</span>
+        <span>🔴 {rangeNames.poor} (&gt; {metricData.range[1]}{unit})</span>
+    </p>
 </div>
 
 <style>
-  div {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    gap: 32px;
-  }
-  .legend {
-    text-align: right;
-    padding: 10px 0;
-  }
-
-  .legend span {
-    white-space: nowrap;
-  }
-  .desktop{
-    display: inline-block;
-  }
-  .mobile{
-    display: none;
-  }
-
-  @media (max-width: 1022px) {
+    div {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        gap: 32px;
+    }
     .legend {
-      text-align: left;
-      font-size: 80%;
+        text-align: right;
+        padding: 10px 0;
     }
-    .desktop{
-      display: none;
-    }
-    .mobile{
-    display: inline-block;
-  }
 
-  }
+    .legend span {
+        white-space: nowrap;
+    }
+    .desktop {
+        display: inline-block;
+    }
+    .mobile {
+        display: none;
+    }
+
+    @media (max-width: 1022px) {
+        .legend {
+            text-align: left;
+            font-size: 80%;
+        }
+        .desktop {
+            display: none;
+        }
+        .mobile {
+            display: inline-block;
+        }
+    }
 </style>
