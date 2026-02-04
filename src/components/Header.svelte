3d3851de02f58f1d7d5a08ac9aeb6051ad85a77e
gotomi <gotomi@gotomi.info>
2026-02-04 19:07:13 +0100

    fix date formatting

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,8 +3,8 @@
     const params = $derived(data.params);
     const urlType = $derived(params.url ? "URL" : "ORIGIN");
     const {firstDate, lastDate} = $derived(data.params.collectionPeriod);
-    const firstDay = $derived(new Date(firstDate.year ,firstDate.month,firstDate.day).toLocaleDateString('en-UK',{format: 'short'}));
-    const lastDay = $derived(new Date(lastDate.year ,lastDate.month,lastDate.day).toLocaleDateString('en-UK',{format: 'short'}));
+    const firstDay = $derived(new Date(Date.UTC(firstDate.year ,firstDate.month - 1,firstDate.day)).toLocaleDateString('en-UK',{format: 'short'}));
+    const lastDay = $derived(new Date(Date.UTC(lastDate.year ,lastDate.month - 1,lastDate.day)).toLocaleDateString('en-UK',{format: 'short'}));
 
 </script>
 
